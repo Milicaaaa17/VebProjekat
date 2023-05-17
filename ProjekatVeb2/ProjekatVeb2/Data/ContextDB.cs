8aa0e52f60cd917df0f1e00a6edf34d1fcb7109c
Milicaaaa17 <milicar.506@gmail.com>
2023-05-17 15:54:24 +0200

    Konekcija sa bazom

---
+++ ProjekatVeb2/ProjekatVeb2/Data/ContextDB.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjekatVeb2.Data
+{
+    public class ContextDB : DbContext
+    {
+        public ContextDB(DbContextOptions<ContextDB> options) : base(options)
+        {
+
+        }
+    }
+}
